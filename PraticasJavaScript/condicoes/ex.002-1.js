d9f9e0f534b311403f70b27cb7bba91d9f2c055b
Lucas Sampaio <sampaiolucas58@gmail.com>
2022-02-23 20:26:18 -0300

    primeiro commit commit

---
+++ PraticasJavaScript/condicoes/ex.002-1.js
@@ -0,0 +1,8 @@
+var vel = 77
+var via = 80
+console.log('A velocidade do seu carro é ' + vel + 'km/h')
+if (vel > 80) {
+    console.log(`Voçê ultrapassou a velocidade permitida de ${via} KM/h`)
+    console.log(`Multa de 50 reais pela infração`)
+}
+console.log(`Velocidade está dentro do permitido de ${via} KM/h`)
---
+++ PraticasJavaScript/condicoes/ex.003.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Você mora no ${pais}`)
+if (pais == 'Brasil') {
+    console.log(`você é brasileiro`)
+}else {
+    console.log('você é estrangeiro')
+}
